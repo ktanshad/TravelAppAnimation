945d018709fb328ba2875c804207dc994bfb6d4b
anshad KT <anshadaze@gmail.com>
2023-10-27 15:53:40 +0530

    commit

--- lib/controller/tripprovider.dart
+++ lib/controller/tripprovider.dart
@@ -1,54 +1,25 @@
-
 import 'package:flutter/material.dart';
-import 'package:travelappanimation/models/Trip.dart';
+import 'package:travelappanimation/services/tripservices.dart';
 import 'package:travelappanimation/view/home_screen/widgets/trip_packagetile.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class TripProvider extends ChangeNotifier {
-
-
   final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
-  List tripPackageTiles = [];
-  void fetchTrips(BuildContext context) {
+  List<TripPackageTile> tripPackageTiles = [];
+  final TripService _tripService = TripService();
+
+  Future<void> fetchTrips(BuildContext context) async {
     tripPackageTiles.clear();
-    List<TripModel> trips = [
-      TripModel(
-          title:AppLocalizations.of(context)!.munnartitle,
-          price: '350',
-          nights: '3',
-          img: 'munnar.jpg',
-          details: AppLocalizations.of(context)!.munnardetails),
-      TripModel(
-          title: AppLocalizations.of(context)!.dubaititle,
-          price: '400',
-          nights: '5',
-          img: 'dubai.jpg',
-          details:AppLocalizations.of(context)!.dubaidetails),
-      TripModel(
-          title: AppLocalizations.of(context)!.japantitle,
-          price: '750',
-          nights: '2',
-          img: 'japan.jpg',
-          details:AppLocalizations.of(context)!.japandetails),
-      TripModel(
-          title: AppLocalizations.of(context)!.kashmirtitle,
-          price: '600',
-          nights: '4',
-          img: 'kashmir.jpg',
-          details:AppLocalizations.of(context)!.kashmirdetails),
-    ];
+    final trips = _tripService.fetchTrips(context);
 
-   Future ft = Future(() {});
-   trips.forEach((TripModel trip) {
-      ft = ft.then((value) {
-        return Future.delayed(const Duration(milliseconds: 100), () {
-          tripPackageTiles.add(TripPackageTile(context: context, trip: trip));
-          listKey.currentState!.insertItem(tripPackageTiles.length - 1);
-        });
-      });
+    final ft = Future(() {});
+    for (final trip in trips) {
+      await ft;
+      await Future.delayed(const Duration(milliseconds: 100));
+      tripPackageTiles.add(TripPackageTile(context: context, trip: trip));
+      listKey.currentState!.insertItem(tripPackageTiles.length - 1);
       notifyListeners();
-    });
+    }
   }
-
 }
+
 
--- lib/services/tripservices.dart
+++ lib/services/tripservices.dart
@@ -1,16 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:travelappanimation/models/Trip.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+class TripService {
+  // This function simulates fetching trips from an API or database.
+  List<TripModel> fetchTrips(BuildContext context) {
+    // Replace this with your actual data-fetching logic.
+    return [
+      TripModel(
+        title: AppLocalizations.of(context)!.munnartitle,
+        price: '350',
+        nights: '3',
+        img: 'munnar.jpg',
+        details: AppLocalizations.of(context)!.munnardetails,
+      ),
+      TripModel(
+        title: AppLocalizations.of(context)!.dubaititle,
+        price: '400',
+        nights: '5',
+        img: 'dubai.jpg',
+        details: AppLocalizations.of(context)!.dubaidetails,
+      ),
+      TripModel(
+        title: AppLocalizations.of(context)!.japantitle,
+        price: '750',
+        nights: '2',
+        img: 'japan.jpg',
+        details: AppLocalizations.of(context)!.japandetails,
+      ),
+      TripModel(
+        title: AppLocalizations.of(context)!.kashmirtitle,
+        price: '600',
+        nights: '4',
+        img: 'kashmir.jpg',
+        details: AppLocalizations.of(context)!.kashmirdetails,
+      ),
+    ];
+  }
+}
 
-// import 'package:travelappanimation/models/Trip.dart';
-// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-
-// // class TripServices{
-// //   // get data from db
-  
-
- 
-
-// //   }
 
 
 
